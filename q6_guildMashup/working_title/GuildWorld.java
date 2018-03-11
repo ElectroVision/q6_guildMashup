3bab75f828e4db0747d9b2b87d59ee402b63a10e
James <36755477+ElectroVision@users.noreply.github.com>
2018-03-10 23:33:14 -0800

    Add files via upload

---
+++ q6_guildMashup/working_title/GuildWorld.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class GuildWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GuildWorld extends World
+{
+
+    /**
+     * Constructor for objects of class GuildWorld.
+     * 
+     */
+    public GuildWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
